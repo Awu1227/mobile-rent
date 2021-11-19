7e196eaeaec7c09f67d24607b6665c4891b6f79a
Orange <2497436726@qq.com>
2021-11-19 19:46:41 +0800

    :rocket: 项目准备

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 // 导入路由
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// 导入样式
+import 'antd-mobile/dist/antd-mobile.css';
 import './index.css';
 import App from './App';
 
