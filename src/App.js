4a45e36fedd00292245ef90ad0f70765a5ca527d
Orange <2497436726@qq.com>
2021-11-19 19:11:24 +0800

    项目准备

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+// 导入路由
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+// 导入首页和城市选择两个组件（页面）
+import Home from './pages/Home'
+import CityList from './pages/ListCity'
+
+// 导入要使用的组件
+import { Button } from 'antd-mobile';
 
 function App() {
   return (
-    <div className="App">
-      项目的根组件
-    </div>
+    <Router>
+      <div className="App">
+        项目的根组件<Button>start</Button>
+
+        {/* 配置导航菜单 */}
+
+        <Link to="/home">首页</Link>
+        <Link to="/citylist">城市选择页</Link>
+        {/*配置路由*/}
+        <Route path="/home" component={Home}></Route>
+        <Route path="/citylist" component={CityList}></Route>
+      </div>
+    </Router>
   );
 }
 
