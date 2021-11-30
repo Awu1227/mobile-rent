70f9f5e40a474667848cfd53451c973c635c9b3b
Orange <2497436726@qq.com>
2021-11-30 11:13:01 +0800

    :white_check_mark:首页路由处理

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,7 +15,7 @@ const tabItems = [
   {
     title: "首页",
     icon: "icon-ind",
-    path: "/home/index",
+    path: "/home",
   },
   {
     title: "找房",
@@ -61,12 +61,11 @@ export default class Home extends React.Component {
     ));
   }
   render() {
-    console.log(this.props.location.pathname);
     return (
       <div className="home">
         {/*渲染子路由 */}
         <Route path="/home/news" component={News} />
-        <Route path="/home/index" component={Index} />
+        <Route exact path="/home" component={Index} />
         <Route path="/home/list" component={HouseList} />
         <Route path="/home/profile" component={Profile} />
 
