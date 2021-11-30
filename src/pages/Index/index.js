ce957e89fb6f27c03c8bbac334ec9b57e0f7d793
Orange <2497436726@qq.com>
2021-11-30 16:00:37 +0800

    :triangular_flag_on_post:获取轮播图数据

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -2,45 +2,52 @@ import React from "react";
 
 // 导入组件
 import { Carousel } from "antd-mobile";
+
+// 导入axios
+import axios from "axios";
 export default class Index extends React.Component {
   state = {
-    // 图片的名称
-    data: ["1", "2", "3"],
+    // 轮播图状态数据
+    swipers: [],
     imgHeight: 212,
   };
+
+  // 获取轮播图数据的方法
+  async getSwipers() {
+    const res = await axios.get("http://localhost:8080/home/swiper");
+    this.setState({
+      swipers: res.data.body,
+    });
+    console.log(res);
+  }
   componentDidMount() {
-    // simulate img loading
-    setTimeout(() => {
-      this.setState({
-        data: [
-          "AiyWuByWklrrUDlFignR",
-          "TekJlZRVCjLFexlOCuWn",
-          "IJOtIlfsYdTyaDTRVrLI",
-        ],
-      });
-    }, 100);
+    this.getSwipers();
+  }
+  // 渲染轮播图结构
+  renderSwipers() {
+    return this.state.swipers.map(item => (
+      <a
+        key={item.id}
+        href="https://zh-hans.reactjs.org/"
+        style={{
+          display: "inline-block",
+          width: "100%",
+          height: this.state.imgHeight,
+        }}
+      >
+        <img
+          src={`http://localhost:8080${item.imgSrc}`}
+          alt=""
+          style={{ width: "100%", verticalAlign: "top" }}
+        />
+      </a>
+    ));
   }
   render() {
     return (
       <div className="index">
         <Carousel autoplay={true} infinite>
-          {this.state.data.map(val => (
-            <a
-              key={val}
-              href="https://zh-hans.reactjs.org/"
-              style={{
-                display: "inline-block",
-                width: "100%",
-                height: this.state.imgHeight,
-              }}
-            >
-              <img
-                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-                alt=""
-                style={{ width: "100%", verticalAlign: "top" }}
-              />
-            </a>
-          ))}
+          {this.renderSwipers()}
         </Carousel>
       </div>
     );
