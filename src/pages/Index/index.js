d5b22262dca811e2648cd0e5593913c96b307283
Orange <2497436726@qq.com>
2021-12-08 10:49:09 +0800

    :recycle:导航菜单代码重构

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -14,6 +14,34 @@ import Nav4 from "../../assets/images/nav-4.png";
 
 // 导入样式文件
 import "./index.css";
+
+// 导航菜单数据
+const navs = [
+  {
+    id: 1,
+    img: Nav1,
+    title: "整租",
+    path: "/home/list",
+  },
+  {
+    id: 2,
+    img: Nav2,
+    title: "合租",
+    path: "/home/list",
+  },
+  {
+    id: 3,
+    img: Nav3,
+    title: "地图找房",
+    path: "/map",
+  },
+  {
+    id: 4,
+    img: Nav4,
+    title: "去出租",
+    path: "/home/list",
+  },
+];
 export default class Index extends React.Component {
   state = {
     // 轮播图状态数据
@@ -52,6 +80,18 @@ export default class Index extends React.Component {
       </a>
     ));
   }
+  // 渲染导航菜单
+  renderNavs() {
+    return navs.map(item => (
+      <Flex.Item
+        key={item.id}
+        onClick={() => this.props.history.push(item.path)}
+      >
+        <img src={item.img} alt=""></img>
+        <h2>{item.title}</h2>
+      </Flex.Item>
+    ));
+  }
   render() {
     return (
       <div className="index">
@@ -60,24 +100,7 @@ export default class Index extends React.Component {
           {this.renderSwipers()}
         </Carousel>
         {/*导航菜单*/}
-        <Flex className="nav">
-          <Flex.Item>
-            <img src={Nav1} alt=""></img>
-            <h2>整租</h2>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav2} alt=""></img>
-            <h2>合租</h2>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav3} alt=""></img>
-            <h2>地图找房</h2>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav4} alt=""></img>
-            <h2>去出租</h2>
-          </Flex.Item>
-        </Flex>
+        <Flex className="nav">{this.renderNavs()}</Flex>
       </div>
     );
   }
