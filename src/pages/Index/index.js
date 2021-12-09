649965d6149333f6d77f59d843095b8b7c873d63
Orange <2497436726@qq.com>
2021-12-09 14:58:51 +0800

    :lipstick:租房小组样式完成

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 // 导入组件
-import { Carousel, Flex } from "antd-mobile";
+import { Carousel, Flex, Grid } from "antd-mobile";
 
 // 导入axios
 import axios from "axios";
 
-// 导入图片
+// 导入导航菜单图片
 import Nav1 from "../../assets/images/nav-1.png";
 import Nav2 from "../../assets/images/nav-2.png";
 import Nav3 from "../../assets/images/nav-3.png";
@@ -39,27 +39,18 @@ const navs = [
     id: 4,
     img: Nav4,
     title: "去出租",
-    path: "/home/list",
+    path: "/rent",
   },
 ];
 
-/*
-  轮播图存在的两个问题：
-  1. 不会自动播放
-  2. 从其他路由返回的时候，高度不够
-
-  原因：轮播图数据是动态加载的，加载完成前后轮播图数量不一致导致的问题
-
-  如何解决？
-  1. 在state中添加表示轮播图加载完成的数据
-  2. 在轮播图数据加载完成时修改该数据状态值为true
-  3. 只有在轮播图数据加载完成的情况下，才渲染轮播图组件
-*/
 export default class Index extends React.Component {
   state = {
     // 轮播图状态数据
     swipers: [],
     isSwiperLoaded: false,
+
+    // 租房小组数据
+    groups: [],
   };
 
   // 获取轮播图数据的方法
@@ -70,15 +61,32 @@ export default class Index extends React.Component {
       isSwiperLoaded: true,
     });
   }
+
+  // 获取租房小组数据的方法
+  async getGroups() {
+    const res = await axios.get("http://localhost:8080/home/groups", {
+      params: {
+        area: "AREA%7C88cff55c-aaa4-e2e0",
+      },
+    });
+
+    // console.log(res)
+    this.setState({
+      groups: res.data.body,
+    });
+  }
+
   componentDidMount() {
     this.getSwipers();
+    this.getGroups();
   }
+
   // 渲染轮播图结构
   renderSwipers() {
     return this.state.swipers.map(item => (
       <a
         key={item.id}
-        href="https://zh-hans.reactjs.org/"
+        href="http://itcast.cn"
         style={{
           display: "inline-block",
           width: "100%",
@@ -93,6 +101,7 @@ export default class Index extends React.Component {
       </a>
     ));
   }
+
   // 渲染导航菜单
   renderNavs() {
     return navs.map(item => (
@@ -100,26 +109,63 @@ export default class Index extends React.Component {
         key={item.id}
         onClick={() => this.props.history.push(item.path)}
       >
-        <img src={item.img} alt=""></img>
+        <img src={item.img} alt="" />
         <h2>{item.title}</h2>
       </Flex.Item>
     ));
   }
+
   render() {
     return (
       <div className="index">
-        {/*轮播图*/}
+        {/* 轮播图 */}
         <div className="swiper">
           {this.state.isSwiperLoaded ? (
-            <Carousel autoplay={true} infinite>
+            <Carousel autoplay infinite autoplayInterval={5000}>
               {this.renderSwipers()}
             </Carousel>
           ) : (
             ""
           )}
         </div>
-        {/*导航菜单*/}
+
+        {/* 导航菜单 */}
         <Flex className="nav">{this.renderNavs()}</Flex>
+
+        {/* 租房小组 */}
+        <div className="group">
+          <h3 className="group-title">
+            租房小组 <span className="more">更多</span>
+          </h3>
+
+          {/* 宫格组件 */}
+          <Grid
+            data={this.state.groups}
+            columnNum={2}
+            square={false}
+            hasLine={false}
+            renderItem={item => (
+              <Flex className="group-item" justify="around" key={item.id}>
+                <div className="desc">
+                  <p className="title">{item.title}</p>
+                  <span className="info">{item.desc}</span>
+                </div>
+                <img src={`http://localhost:8080${item.imgSrc}`} alt="" />
+              </Flex>
+            )}
+          />
+        </div>
+
+        {/* <Flex className="group-item" justify="around">
+          <div className="desc">
+            <p className="title">家住回龙观</p>
+            <span className="info">归属的感觉</span>
+          </div>
+          <img
+            src="http://localhost:3000/static/media/group-1.263b84b0.png"
+            alt=""
+          />
+        </Flex> */}
       </div>
     );
   }
